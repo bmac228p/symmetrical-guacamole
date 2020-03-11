7952445b81f554d604d64f8624fc5d68d369ca5b
Jonathan Yeh <jmyeh51@gmail.com>
2020-03-11 11:47:46 -0700

    Updated naming conventions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Page from './Components/Routing';
+import Page from './Components/UniqueLinks';
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
--- src/Components/UniqueLinks.js
+++ src/Components/UniqueLinks.js
@@ -4,25 +4,17 @@ import {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
 
 function Page() {
-    const db = firebase.firestore();
     const [items, setItems] = useState([]);
 
-    useEffect(() => {(db.collection("events").get().then(items => {
-          // doc.data() is never undefined for query doc snapshots
+    useEffect(() => {
+          const db = firebase.firestore();
+          (db.collection("events").get().then(items => {
           const docData = items.docs;
           const data = docData.map(doc => doc.id);
           setItems(data);
         console.log(data);
-
-
   }));
-
-}, []);
-  
- 
-  
-;
-    
+}, []);    
 
   return (
     <div>
